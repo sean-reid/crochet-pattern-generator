ffb44717e540bcb0b21892ed05f05e4a7d333cf5
Sean Reid <seanreid.mail@gmail.com>
2025-12-30 23:58:16 -0800

    fix

---
+++ docs/wasm/crochet_wasm.d.ts
@@ -0,0 +1,55 @@
+/* tslint:disable */
+/* eslint-disable */
+
+/**
+ * Generate a crochet pattern from JSON input
+ */
+export function generate_pattern_from_json(profile_json: string, config_json: string): string;
+
+export function init_panic_hook(): void;
+
+/**
+ * Validate a configuration
+ */
+export function validate_config(config_json: string): string;
+
+/**
+ * Validate a profile curve
+ */
+export function validate_profile(profile_json: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly generate_pattern_from_json: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly init_panic_hook: () => void;
+  readonly validate_config: (a: number, b: number, c: number) => void;
+  readonly validate_profile: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_export2: (a: number, b: number) => number;
+  readonly __wbindgen_export3: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ docs/wasm/crochet_wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const generate_pattern_from_json: (a: number, b: number, c: number, d: number, e: number) => void;
+export const init_panic_hook: () => void;
+export const validate_config: (a: number, b: number, c: number) => void;
+export const validate_profile: (a: number, b: number, c: number) => void;
+export const __wbindgen_export: (a: number, b: number, c: number) => void;
+export const __wbindgen_export2: (a: number, b: number) => number;
+export const __wbindgen_export3: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
